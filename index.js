cf5c4d3c9b5f380f87e39da962ef3ef9f1507057
Rick <ricksmycodeguy@gmail.com>
2021-09-26 10:53:20 -0500

    updating index.js for backend requests

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-// const data = require('/data/db.js');
 const db = require('./data/db.js');
-
 const server = express();
 // server.use(express.json())
 
@@ -12,6 +10,14 @@ server.get('/users', (req, res) => {
         })
         .catch(res.status(500), error => console.log(error));
 
+})
+
+server.post('/users', (req, res)=> {
+    db.insert()
+    .then(user => {
+        res.status(201).json({url:'/users', operation: 'POST', user});
+    })
+    .catch(res.status(500), error => console.log(error))
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
