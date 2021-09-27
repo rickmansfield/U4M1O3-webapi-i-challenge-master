22655d74b5f31a2d00b542da42dcf04804c6e379
Rick <ricksmycodeguy@gmail.com>
2021-09-27 08:45:28 -0500

    Stubbed out CRUD in index.js

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require('express');
-const db = require('./data/db.js');
+const User = require('./data/db.js');
 const server = express();
-// server.use(express.json())
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'Sanity Test. You Are Sane!!' })
+})
 
 server.get('/users', (req, res) => {
-    db.find()
+    User.find()
         .then(user => {
             res.status(200).json(user);
         })
         .catch(res.status(500), error => console.log(error));
-
 })
-//THis DOES NOT WORK AS IS
-// server.post('/users', (req, res)=> {
-//     db.insert()
-//     .then(user => {
-//         res.status(201).json({url:'/users', operation: 'POST', user});
-//     })
-//     .catch(res.status(500), error => console.log(error))
-// })
 
-server.listen(8000, () => console.log('API running on port 8000'));
+server.get('/users/:id', (req, res) => {
+    res.status(200).json({ message: 'user/:id working!!' })
+})
+
+server.post('/users', (req, res) => {
+    const newUser = req.body
+    User.insert(newUser)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: err.message })
+        })
+})
+
+server.put('/users/:id', (req, res) => {
+    res.json({message: 'PUT update to existing user working'})
+})
+
+server.delete('/users/:id', (req, res) =>{
+    res.json({message: `DELETE dog with id ${req.params.id}`})
+})
+
+
+server.listen(6000, () => console.log('API running on port 6000'));
